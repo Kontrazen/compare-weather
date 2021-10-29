36ac0ddcca4aa295e74154a6e72d3fb72daca9fc
Kontrazen <kontrazen@gmail.com>
2021-10-29 18:43:20 +0200

    Add create-react-app template base

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.scss';
+
+import WeatherGroup from './WeatherLocations/WeatherGroup.js/WeatherGroup';
+
+function App() {
+  return (
+    <div className="App">
+		<WeatherGroup/>
+    </div>
+  );
+}
+
+export default App;
